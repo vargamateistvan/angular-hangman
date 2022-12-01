68b6778e50041583130a39b986eab2ced32898e0
MateIstvanVarga <mateistvanvarga@gmail.com>
2022-12-01 11:23:24 +0100

    Init routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GameComponent } from './components/game/game.component';
+import { InstructionsComponent } from './components/instructions/instructions.component';
+import { LengthPickerComponent } from './components/length-picker/length-picker.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'instructions',
+    component: InstructionsComponent,
+  },
+  {
+    path: 'length-picker',
+    component: LengthPickerComponent,
+  },
+  {
+    path: 'hangman',
+    component: GameComponent,
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'instructions',
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
