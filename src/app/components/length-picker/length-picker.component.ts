ecd402290cca5f12fe3b2fbfdab5bfed2bb55d53
MateIstvanVarga <mateistvanvarga@gmail.com>
2022-12-01 13:02:21 +0100

    Length Picker

--- src/app/components/length-picker/length-picker.component.ts
+++ src/app/components/length-picker/length-picker.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GameService } from 'src/app/services/game.service';
 
 @Component({
   selector: 'app-length-picker',
   templateUrl: './length-picker.component.html',
-  styleUrls: ['./length-picker.component.scss']
+  styleUrls: ['./length-picker.component.scss'],
 })
-export class LengthPickerComponent {
+export class LengthPickerComponent implements OnInit {
+  public wordLengths: number[] = [];
+  public selectedWordLength: number = 0;
 
+  constructor(private gameService: GameService) {}
+
+  ngOnInit(): void {
+    this.wordLengths = this.gameService.getWordLengths();
+  }
+
+  public setSelectedWordLength(wordLength: number): void {
+    this.gameService.setWordLength(wordLength);
+    this.selectedWordLength = wordLength;
+  }
+
+  public setRandomSelectedWordLength(): void {
+    const randomLength =
+      this.wordLengths[Math.floor(Math.random() * this.wordLengths.length)];
+    console.log('random', randomLength);
+    this.setSelectedWordLength(randomLength);
+  }
 }
--- src/app/services/game.service.ts
+++ src/app/services/game.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import * as data from '../../assets/hangman_words.json';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class GameService {
+  private words = (data as any).default;
+  private selectedWordLength: number = 0;
 
-  constructor() { }
+  constructor() {}
+
+  getWordLengths(): number[] {
+    const allWordLength: number[] = this.words.map(
+      (word: string) => word.length
+    );
+    return [...new Set(allWordLength)];
+  }
+
+  setWordLength(wordLength: number): void {
+    this.selectedWordLength = wordLength;
+  }
 }
